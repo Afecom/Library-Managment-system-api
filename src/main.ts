6ede25c061c427a2ae9b42edde52cf7facc8e940
Nuru <knuru472@gmail.com>
2025-08-01 21:34:19 +0300

    fixed CORS issue

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*'
+    origin: 'https://library-managment-system-frontend-omega.vercel.app',
+    credentials: true, // Optional: allow cookies, authorization headers
   });
 
   // Enable validation
